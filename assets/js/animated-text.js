22422ec29c1391c9a387fb3bdb26cde55c50014c
Wale Ayandiran <wale.ayandiran@poisenigeria.org>
2017-09-20 11:52:21 +0100

    updates added

--- assets/js/animated-text.js
+++ assets/js/animated-text.js
@@ -1,11 +1,11 @@
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 2500,
+	var animationDelay = 7500,
 		//loading bar effect
 		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
-		lettersDelay = 50,
+		lettersDelay = 500,
 		//type effect
 		typeLettersDelay = 150,
 		selectionDuration = 500,
